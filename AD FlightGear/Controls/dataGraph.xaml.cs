dbd46b027b49713dbd3ab18080cd024c67743572
azran <azranohad@gmail.com>
2021-04-13 11:00:53 +0300

    update design

--- AD FlightGear/Controls/dataGraph.xaml.cs
+++ AD FlightGear/Controls/dataGraph.xaml.cs
@@ -27,9 +27,9 @@ namespace AD_FlightGear.Controls
         private int length;
         public string Title_Left { get; set; }
         public Button selectedItem { get; set; }
-        
 
-        
+
+
         public IList<Button> buttons { get; set; }
 
         private graphs_vm graphs_VM;
@@ -44,8 +44,8 @@ namespace AD_FlightGear.Controls
         {
             InitializeComponent();
             buttons = new List<Button>();
-          //  buttons = graphs_VM.VM_DBflight._ListFeature;
-           // data_list.ItemsSource = graphs_VM.VM_DBflight._ListFeature; 
+            //  buttons = graphs_VM.VM_DBflight._ListFeature;
+            // data_list.ItemsSource = graphs_VM.VM_DBflight._ListFeature; 
         }
         /*
         public void addButtons()
@@ -68,18 +68,24 @@ namespace AD_FlightGear.Controls
         */
         public void data_list_MouseDoubleClick(Object sender, MouseButtonEventArgs e)
         {
-            /*
-            if (data_list.SelectedItem != null)
-            {
-                object selectedItem_object = data_list.SelectedItem;
-                selectedItem = (Button)selectedItem_object;
-                graphs_VM.VM_ChooseIndex = int.Parse(selectedItem.ButtonID);
-                graphs_VM.DataPoints_6(int.Parse(selectedItem.ButtonID));
 
-
-                //graphs_VM.VM_C.updateChoose(graphs_VM.VM_PointsRun, graphs_VM.VM_PointsReg, graphs_VM.VM_TimeInt);
-            }
         }
+        /*
+        if (data_list.SelectedItem != null)
+        {
+        */
+        /*
+            object selectedItem_object = data_list.SelectedItem;
+    selectedItem = (Button) selectedItem_object;
+    graphs_VM.VM_ChooseIndex = int.Parse(selectedItem.ButtonID);
+    graphs_VM.DataPoints_6(int.Parse(selectedItem.ButtonID));
+
+
+            //graphs_VM.VM_C.updateChoose(graphs_VM.VM_PointsRun, graphs_VM.VM_PointsReg, graphs_VM.VM_TimeInt);
+        */
+
+
+
 
 
         private void ic_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -99,14 +105,14 @@ namespace AD_FlightGear.Controls
                 graphs_VM.VM_ChooseIndex = int.Parse(selectedItem.ButtonID);
                 graphs_VM.DataPoints_6(int.Parse(selectedItem.ButtonID));
 
-             //   try
-             //   {
-                    graphs_VM.VM_C.updateChoose(graphs_VM.VM_PointsRun, graphs_VM.VM_PointsReg, graphs_VM.VM_TimeInt);
-             //   }
-            //    catch
-            //    {
+                //   try
+                //   {
+                // graphs_VM.VM_C.updateChoose(graphs_VM.VM_PointsRun, graphs_VM.VM_PointsReg, graphs_VM.VM_TimeInt);
+                //   }
+                //    catch
+                //    {
 
-            //    }
+                //    }
             }
         }
 
@@ -119,7 +125,6 @@ namespace AD_FlightGear.Controls
             {
                 Assembly dll = Assembly.LoadFile(graphs_VM.VM_PathDll);
                 Type[] type = dll.GetExportedTypes();
-                
                 foreach (Type t in type)
                 {
                     if (t.Name == "Graph_I")
@@ -132,7 +137,7 @@ namespace AD_FlightGear.Controls
             catch (Exception e)
             {
                 Console.WriteLine("Error load dll", e);
-            } 
+            }
         }
         private void Button_dll(object sender, RoutedEventArgs e)
         {
@@ -170,7 +175,7 @@ namespace AD_FlightGear.Controls
             {
                 graphs_VM.VM_PathCsv = openFileDialog.FileNames[0];
             }
-            graphs_VM.VM_PathDll = @"C:\Users\Amit\source\repos\circle\circle\bin\Debug\circle.dll";
+            graphs_VM.VM_PathDll = @"C:\Users\azran\source\repos\circle\circle\bin\Debug\circle.dll";
             initializeDll();
 
             graphs_VM.initDBrun();
@@ -181,11 +186,13 @@ namespace AD_FlightGear.Controls
 
             }
             data_list.ItemsSource = buttons;
-        }
 
-        private void data_list_MouseEnter(object sender, MouseEventArgs e)
-        {
+            /*
+            public void data_list_MouseEnter(object sender, MouseEventArgs e)
+            {
 
+            }
+            */
         }
     }
 }
